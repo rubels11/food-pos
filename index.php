5aeb6c102f3a768bfafee13735fb1600f76349f3
Mahmud Hasan <rubelhasanmahmud@gmail.com>
2023-06-13 00:38:21 +0600

    Adding files for savings

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />    
+    
+    <title>Food POS Admin</title>
+</head>
+<body>
+    <section id="sidebar">
+        <a href="#" class="brand">
+            <i class="fa-solid fa-utensils"></i>
+        <span class="text">Food POS</span>
+        </a>
+        <ul class="side-menu top">
+<li>
+    <a href="#"> 
+    <i class="fa-solid fa-chart-pie"></i>
+    <span class="text">Dashboard</span>
+    </a>
+</li>
+<li>
+    <a href="#"> 
+    <i class="fa-solid fa-bowl-food"></i>
+    <span class="text">Cuisine</span>
+    </a>
+</li>
+<li>
+    <a href="#"> 
+    <i class="fa-solid fa-book"></i>
+    <span class="text">Orders</span>
+    </a>
+</li>
+<li>
+    <a href="#"> 
+    <i class="fa-regular fa-user"></i>
+    <span class="text">Users</span>
+    </a>
+</li>
+<li>
+    <a href="#"> 
+    <i class="fa-solid fa-gear"></i>
+    <span class="text">Settings</span>
+    </a>
+</li>
+<li>
+    <a href="#"> 
+    <i class="fa-solid fa-right-from-bracket"></i>
+    <span class="text">Logout</span>
+    </a>
+</li>
+
+        </ul>
+
+
+
+    </section>
+</body>
+</html>
